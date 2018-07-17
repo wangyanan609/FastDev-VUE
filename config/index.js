0a42962b941b716c7eed8391742d7cf342fd2589
wangyanan609 <wangyanan609@qq.com>
2018-07-17 22:14:19 +0800

    修改启动IP为localhost

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
   },
 
     // Various Dev Server settings
-    host: '192.168.56.49', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 9001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     openPage: 'login.html',// 启动页面
